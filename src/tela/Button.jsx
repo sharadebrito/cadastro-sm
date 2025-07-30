434759da3f01de143376a82efe602991fd198c02
Shara de Brito <sharadebrito@hotmail.com>
2025-07-30 12:19:37 -0300

    remove files

--- src/tela/Button.jsx
+++
@@ -1,9 +0,0 @@
-export function Button(criar) {
-  return (
-    <div>
-      <button onClick={criar}>
-        <b>Cadastrar</b>
-      </button>
-    </div>
-  );
-}
--- src/tela/MsgFinish.jsx
+++
@@ -1,9 +0,0 @@
-export function MsgFinish() {
-  return (
-    <div>
-      <a href="https://www.manager-sportings.com.br/login">
-        Já tem uma conta? Entrar
-      </a>
-    </div>
-  );
-}
